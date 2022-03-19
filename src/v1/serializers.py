3f88648527fc439a09c7122731225b7eee3dc8c1
unknown <diagaaos@gmail.com>
2022-03-19 17:54:19 +0500

    feat: add serializers

---
+++ src/v1/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework.serializers import ModelSerializer, ManyRelatedField
+from . import models
+
+
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        model = models.Topic
+        fields = ('id', 'short_descriptor', 'long_descriptor')
+
+
+class FolderSerializer(ModelSerializer):
+    class Meta:
+        model = models.Folder
+        fields = ('id', 'name', 'folder', 'topics')
+
+
+class DocumentSerializer(ModelSerializer):
+    class Meta:
+        model = models.Document
+        fields = ('id', 'title', 'content', 'folder', 'topics')
+
+
+class FolderTopicSerializer(ModelSerializer):
+    class Meta:
+        model = models.FolderTopic
+        fields = ('id', 'folder', 'topic')
+
+
+class DocumentTopicSerializer(ModelSerializer):
+    class Meta:
+        model = models.DocumentTopic
+        fields = ('id', 'document', 'topic')
