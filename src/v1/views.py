f1adfefd5f06ec52b6602512838ec0ff4cb446d7
unknown <diagaaos@gmail.com>
2022-03-19 17:54:59 +0500

    feat: add views for all models

--- src/v1/views.py
+++ src/v1/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet, mixins
 
-# Create your views here.
+from . import models
+from . import serializers
+from . import filters
+
+
+class TopicViewSet(GenericViewSet,
+                   mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.ListModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin):
+    queryset = models.Topic.objects.all()
+    serializer_class = serializers.TopicSerializer
+
+
+class FolderViewSet(GenericViewSet,
+                    mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin):
+    queryset = models.Folder.objects.all()
+    serializer_class = serializers.FolderSerializer
+    filterset_class = filters.FolderFilter
+
+
+class DocumentViewSet(GenericViewSet,
+                      mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin):
+    queryset = models.Document.objects.all()
+    serializer_class = serializers.DocumentSerializer
+    filterset_class = filters.DocumentFilter
+
+
+class FolderTopicViewSet(GenericViewSet,
+                         mixins.CreateModelMixin,
+                         mixins.DestroyModelMixin):
+    queryset = models.FolderTopic.objects.all()
+    serializer_class = serializers.FolderTopicSerializer
+
+
+class DocumentTopicViewSet(GenericViewSet,
+                           mixins.CreateModelMixin,
+                           mixins.DestroyModelMixin):
+    queryset = models.DocumentTopic.objects.all()
+    serializer_class = serializers.DocumentTopicSerializer
